577cee6786d5e70687d6bba236415080a26996d4
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-04-28 23:49:29 -0400

    Finish home page design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import {Button, Input} from "flowbite-svelte";
+
+    let uploadCode = "";
+</script>
+
+<div class="flex h-screen items-center justify-center">
+    <div>
+        <div class="bg-gray-800 flex flex-col space-y-4 p-5 rounded-3xl">
+            <h2 class="text-center text-3xl"><span class="text-primary-500">O</span>neUpload</h2>
+            <p class="text-gray-300">To begin uploading a file, please enter your upload code.</p>
+            <Input bind:value={uploadCode} type="text" placeholder="XXXX-XXXX-XXXX" />
+            <Button href='/upload?code={uploadCode}'>Continue</Button>
+        </div>
+    </div>
+</div>
