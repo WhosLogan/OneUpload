ca846763c89ee3bb8ecee4f3923571fb6c8f019a
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-04-29 16:45:54 -0400

    Add basic input validation to the upload code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,21 @@
     import {Button, Input} from "flowbite-svelte";
 
     let uploadCode = "";
+
+    function onCodeType() {
+        // Make sure all characters are uppercase
+        uploadCode = uploadCode.toUpperCase();
+
+        // If the upload code is longer than 12 then remove the last character
+        if (uploadCode.length > 14) {
+            uploadCode = uploadCode.substring(0, uploadCode.length - 1);
+            return;
+        }
+
+        if (uploadCode.replaceAll('-', '').length % 4 === 0 && uploadCode.length !== 14 && uploadCode.length !== 0) {
+            uploadCode += '-';
+        }
+    }
 </script>
 
 <div class="flex h-screen items-center justify-center">
@@ -9,8 +24,8 @@
         <div class="bg-gray-800 flex flex-col space-y-4 p-5 rounded-3xl">
             <h2 class="text-center text-3xl"><span class="text-primary-500">O</span>neUpload</h2>
             <p class="text-gray-300">To begin uploading a file, please enter your upload code.</p>
-            <Input bind:value={uploadCode} type="text" placeholder="XXXX-XXXX-XXXX" />
-            <Button href='/upload?code={uploadCode}'>Continue</Button>
+            <Input on:input={onCodeType} bind:value={uploadCode} type="text" placeholder="XXXX-XXXX-XXXX" />
+            <Button href='/upload?code={uploadCode}' disabled={uploadCode.length !== 14}>Continue</Button>
         </div>
     </div>
 </div>
