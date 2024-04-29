a5b9eb588f02b9067b0bed67cac437a6ca2c1ba5
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-04-28 23:49:17 -0400

    Finish basic website theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        // flowbite-svelte
+        primary: {
+          50: '#FFF5F2',
+          100: '#FFF1EE',
+          200: '#FFE4DE',
+          300: '#FFD5CC',
+          400: '#cfadff',
+          500: '#b276fb',
+          600: '#9852fb',
+          700: '#8b41f6',
+          800: '#822dfa',
+          900: '#7512ff',
+        }
+      }
+    }
   },
-  plugins: []
+  plugins: [require('flowbite/plugin')],
+  darkMode: 'selector',
 };
