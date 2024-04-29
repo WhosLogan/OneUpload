d888add028d4b51f0e9a338411692e965ca16489
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-04-29 16:48:13 -0400

    Remove unused import

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -1,4 +1,4 @@
-import {getXataClient, XataClient} from './xata';
+import {XataClient} from './xata';
 import {XATA_API_KEY} from '$env/static/private'
 
 const database = new XataClient({apiKey: XATA_API_KEY});
