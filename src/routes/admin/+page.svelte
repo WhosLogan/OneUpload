3b4565df978ad681ed39f03030f555f32ee66b05
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-05-01 08:04:52 -0400

    Change the color of the sign in/out buttons

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -9,10 +9,10 @@
         <div class="bg-gray-800 flex flex-col space-y-4 p-5 rounded-3xl">
             <h2 class="text-center text-3xl"><span class="text-primary-500">O</span>neUpload</h2>
             {#if !$page.data.session}
-                <Button on:click={() => signIn("github")}>Sign In with GitHub</Button>
+                <Button color="light" on:click={() => signIn("github")}>Sign In with GitHub</Button>
             {:else}
                 <p class="text-gray-300">You are not permitted to view this page</p>
-                <Button on:click={() => signOut()}>Sign Out</Button>
+                <Button color="light" on:click={() => signOut()}>Sign Out</Button>
             {/if}
         </div>
     </div>
